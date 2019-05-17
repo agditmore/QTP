3bef66a0fc48256fd5038276a11e478e695c8586
Allison Ditmore <allisonditmore@gmail.com>
2019-05-17 12:55:23 -0500

    updating new files

--- HelloWorldNoYarn/main.js
+++ HelloWorldNoYarn/main.js
@@ -1,1 +1,12 @@
 //start here
+console.log("hello");
+
+const App = React.createClass({
+    render() {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+});
+
+React.render(<App />, document.getElementById("root"));
---
+++ jsx-and-components-practice/src/MyAwesomeFirstComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyAwesomeFirstComponent = (props) => (
+    <p>Hello {props.firstName} {props.lastName}</p>
+    );
+
+export default MyAwesomeFirstComponent;
